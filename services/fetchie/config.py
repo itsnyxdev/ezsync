4b1994075c97b7348c82b02589ffefffded52125
Mohammad Falahi <falahideveloper@gmail.com>
2025-05-30 12:07:09 +0330

    refactor(config): update configuration with account credentials
    Add ACCOUNT_EMAIL and ACCOUNT_PASSWORD to .env and config.py
    to centralize credential management.

--- services/fetchie/config.py
+++ services/fetchie/config.py
@@ -5,3 +5,6 @@ load_dotenv()
 
 DRIVER_PATH = os.getenv("DRIVER_PATH")
 DRIVER_PROFILE_PATH = os.getenv("DRIVER_PROFILE_PATH")
+
+ACCOUNT_EMAIL = os.getenv("ACCOUNT_EMAIL")
+ACCOUNT_PASSWORD = os.getenv("ACCOUNT_PASSWORD")
