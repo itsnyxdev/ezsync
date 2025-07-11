094c85699966484d829664a872ce5802cd6ee3a1
Mohammad Falahi <falahideveloper@gmail.com>
2025-07-11 18:45:45 +0330

    ✨  feat: update backend requirements and models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,6 +6,12 @@ from app.models import Base, HasTimestamps
 
 class User(Base, HasTimestamps):
     __tablename__ = "users"
+    __table_args__ = {
+        "mysql_engine": "InnoDB",
+        "mariadb_engine": "InnoDB",
+        "mysql_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_unicode_ci",
+    }
 
     id: Mapped[int] = mapped_column(BigInteger, Identity(), primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
